c1ca2aab6dbc78b650032f0630462499e5391b59
RMIT-Vietnam-Course <154208592+controltheanh52@users.noreply.github.com>
2026-04-14 18:02:19 +0700

    update new version

--- queries_loader.py
+++ queries_loader.py
@@ -4,7 +4,7 @@ def load_queries(filename):
     with open(filename, "r") as f:
         queries = json.load(f)
 
-    # đảm bảo field luôn tồn tại
+    # json files always have content
     for q in queries:
         q.setdefault("avoid_nodes", [])
         q.setdefault("avoid_edges", [])
